1911e0780643750102b551834c900effedf3012a
Dmitrii Kondratev <dimonloboto@gmail.com>
2012-12-30 02:15:35 -0600

    Update

--- lotrWeaponsMod/common/GlowingPhial.java
+++ lotrWeaponsMod/common/GlowingPhial.java
@@ -1,6 +1,14 @@
 package lotrWeaponsMod.common;
 
+import java.util.Random;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
+import net.minecraft.world.storage.MapData;
 
 public class GlowingPhial extends Item {
 
@@ -18,5 +26,42 @@ public class GlowingPhial extends Item {
 	
 	public String getTextureFile() {
 		return ClientProxy.LOTRITEMS_PNG;
+	}
+	public void onUpdate(ItemStack par1ItemStack, World par2World, Entity par3Entity, int par4, boolean par5) {     
+
+            if (par3Entity instanceof EntityPlayer)
+            {
+                EntityPlayer var7 = (EntityPlayer)par3Entity;
+                
+            }
+
+            if (par5)
+            {
+            	
+                int plX = (int)par3Entity.posX;
+                int plY = (int)par3Entity.posY-1;
+                int plZ = (int)par3Entity.posZ;
+                Random rand = new Random();
+                int chance = rand.nextInt(20);
+                if (par2World.getBlockId(plX,  plY,  plZ) == 0){
+                	//par2World.setBlockWithNotify(plX, plY, plZ, lotrWeapons.LightBlock.blockID);
+                	//EnumSkyBlock par1EnumSkyBlock = null;
+                	for (int i = plX-8; i<=(plX+8); i++){
+                		for (int j = plY-8; j <=plY+8; j++){
+                			for (int k = plZ-8; k<=plZ+8; k++){
+                				if (par2World.getBlockId(i,  j,  k) == lotrWeapons.LightBlock.blockID){
+                					par2World.setBlock(i, j, k, 0);
+                					System.out.println("Removed");
+                				}
+                			}
+                		}
+                	}
+                	par2World.setBlockAndMetadataWithUpdate(plX,  plY,  plZ,  lotrWeapons.LightBlock.blockID,  0,  true);
+                	//System.out.println("fail");
+                }
+                
+               
+            }
+        
 	}
 }
--- lotrWeaponsMod/common/LightBlock.java
+++ lotrWeaponsMod/common/LightBlock.java
@@ -5,6 +5,8 @@ import java.util.Random;
 import cpw.mods.fml.client.ITextureFX;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.block.material.Material;
 
@@ -17,10 +19,20 @@ public class LightBlock extends Block
 		this.setCreativeTab(pumpkinMods.common.pumpkinModsBase.pumpkinTabBlocks);
 		this.setLightOpacity(0);
 		this.setLightValue(1.0F);
-		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
-		this.setStepSound(null);
+		this.setBlockBounds(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
 	}
 
+	//@Override
+	
+	public boolean isAirBlock(World world, int x, int y, int z) 
+    {
+        return true;
+    }
+
+    public boolean canBlockStay(World par1World, int par2, int par3, int par4)
+    {
+        return false;
+    }
 	@Override
 	public int getBlockTextureFromSide(int inp)
 	{
@@ -31,7 +43,10 @@ public class LightBlock extends Block
 	{
         return false;
 	}
-
+	 public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int i, int j, int k)
+	    {
+	        return null;
+	    }
 	public int getRenderBlockPass()
 	{
         return 1;
@@ -41,19 +56,30 @@ public class LightBlock extends Block
 	{
         return false;
 	}
-
-protected boolean canSilkHarvest()
-{
+    public boolean isBlockReplaceable(World world, int x, int y, int z) 
+    {
+        return true;
+    }
+	//@Override
+	//public void onBlockAdded(World par1World, int i, int j, int k){
+    //super.onBlockAdded(par1World, i, j, k);
+	//		par1World.setBlock(i, j, k, 0);
+	//		
+		
+	//}
+	
+	protected boolean canSilkHarvest()
+	{
         return false;
-}
-
-public int idDropped(int par1, Random random, int par2) {
-	return 0;
 	}
 
-@Override
-public String getTextureFile () {
-	return ClientProxy.LOTRBLOCKS_PNG;
+	public int idDropped(int par1, Random random, int par2) {
+		return 0;
+	}
+
+	@Override
+	public String getTextureFile () {
+		return ClientProxy.LOTRBLOCKS_PNG;
 	
-}
+	}
 }
